400170e87bbce877d0202ff9196126c2041d7872
iris112 <nglayi99@gmail.com>
2024-06-17 14:17:03 -0500

    working for sturdy-linea-silo subgraph in the goldsky

--- src/helpers/initializer.ts
+++ src/helpers/initializer.ts
@@ -47,7 +47,7 @@ export function getOrInitSturdyPair(address: Address): SturdyPair {
 
     pair.address = address;
     pair.asset = assetToken.id;
-    pair.collateralAsset collateralAssetToken.id;
+    pair.collateralAsset = collateralAssetToken.id;
 
     pair.save();
   }
